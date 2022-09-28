2980dcc56607795cd753c7575e13e8e227a5c045
nicoladaniello <nicola.danie@gmail.com>
2022-09-28 12:24:08 +0200

    add tests for if statements

--- src/__tests__/if.test.ts
+++ src/__tests__/if.test.ts
@@ -4,16 +4,40 @@ import Parser from "../Parser";
 describe("Parser", () => {
   const parser = new Parser();
 
+  it("should support if statement", () => {
+    const ast = parser.parse(`
+        if (x) {
+            x = 1;
+        }
+    `);
+
+    expect(ast).toEqual(
+      Factory.Program([
+        Factory.IfStatement(
+          Factory.Identifier("x"),
+          Factory.BlockStatement([
+            Factory.ExpressionStatement(
+              Factory.AssignmentExpression(
+                "=",
+                Factory.Identifier("x"),
+                Factory.NumericLiteral(1)
+              )
+            ),
+          ]),
+          null
+        ),
+      ])
+    );
+  });
+
   it("should support if-else statement", () => {
     const ast = parser.parse(`
-        
         if (x) {
             x = 1;
         } else {
             x = 2;
         }
-        
-        `);
+    `);
 
     expect(ast).toEqual(
       Factory.Program([
@@ -37,6 +61,48 @@ describe("Parser", () => {
               )
             ),
           ])
+        ),
+      ])
+    );
+  });
+
+  it("should support inline if", () => {
+    const ast = parser.parse(`
+        if (x) x = 1;
+    `);
+
+    expect(ast).toEqual(
+      Factory.Program([
+        Factory.IfStatement(
+          Factory.Identifier("x"),
+          Factory.ExpressionStatement(
+            Factory.AssignmentExpression(
+              "=",
+              Factory.Identifier("x"),
+              Factory.NumericLiteral(1)
+            )
+          ),
+          null
+        ),
+      ])
+    );
+  });
+
+  it("should support consequent ifs", () => {
+    const ast = parser.parse(`
+        if (x) if (y) {} else {} else {}
+    `);
+
+    expect(ast).toEqual(
+      Factory.Program([
+        Factory.IfStatement(
+          Factory.Identifier("x"),
+          Factory.IfStatement(
+            Factory.Identifier("y"),
+            Factory.BlockStatement([]),
+            Factory.BlockStatement([])
+          ),
+          Factory.BlockStatement([])
         ),
       ])
     );
